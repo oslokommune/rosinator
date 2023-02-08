44e744793bc1a3e26559cadfa9b200c0dc3d9fab
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2023-02-08 11:57:03 +0100

    Basic shape of app taking place

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@ export default {
     }
   },
   methods: {
+
+    // on data change
+    onInput() {
+      console.log('onInput');
+    },
     // export text
     exportText() {
       let text = '';
@@ -32,30 +37,79 @@ export default {
       localStorage.setItem('title', title);
       localStorage.setItem('description', description);
       localStorage.setItem('verdier', JSON.stringify(verdier));
+
+
     },
     // load from local storage
     load() {
       this.title = localStorage.getItem('title');
       this.description = localStorage.getItem('description');
       this.verdier = JSON.parse(localStorage.getItem('verdier'));
+    },
+    fullEdit() {
+      let edit = document.getElementById('edit');
+      let preview = document.getElementById('preview');
+      edit.style.width = '195%'
+      edit.style.display = 'block';
+      preview.style.display = 'none';
+    },
+    fullPreview() {
+      let edit = document.getElementById('edit');
+      let preview = document.getElementById('preview');
+      preview.style.width = '195%'
+      preview.style.display = 'block';
+      edit.style.display = 'none';
+    },
+    sideBySide() {
+      let edit = document.getElementById('edit');
+      let preview = document.getElementById('preview');
+      edit.style.width = '';
+      preview.style.width = '';
+      preview.style.display = 'block';
+      edit.style.display = 'block';
+    },
+    toggle(id) {
+      let elem = document.getElementById(id);
+      if (elem.style.display === 'none') {
+        elem.style.display = 'block';
+      } else {
+        elem.style.display = 'none';
+      }
     }
   }
 }
 </script>
 
 <template>
-  <div class="edit">
+
+  <nav>
+    <button @click="exportText">Export text</button>
+    <button @click="exportJson">Export json</button>
+    <button @click="save">Save</button>
+    <button @click="load">Load</button>
+    <button @click="fullEdit">Edit</button>
+    <button @click="fullPreview">Preview</button>
+    <button @click="sideBySide">Side-by-side</button>
+  </nav>
+
+  <div id="edit" class="edit">
   <h2>Overskrift</h2>
-  <input v-model="title">
+  <input class="single" v-model="title">
   <h2>Beskrivelse</h2>
     <textarea v-model="description"></textarea>
     <h2>Verdier</h2>
+    <button @click="toggle('values')">?</button>
+    <p id="values">Verdiene i løsningen man ønsker å beskytte (skrive noe mer om K I T) og hva det betyr</p>
+
+
+
+
     <table>
       <tr>
         <th>Tittel</th>
         <th>Fokus</th>
       </tr>
-      <tr v-for="verdi in verdier" :key="verdi.tittel">
+      <tr v-for="verdi in verdier">
         <td><input v-model="verdi.tittel"></td>
         <td><input v-model="verdi.fokus"></td>
         <td><button @click="verdier.splice(verdier.indexOf(verdi), 1)">X</button></td>
@@ -64,7 +118,7 @@ export default {
   <button @click="verdier.push({tittel: 'tittel', fokus: 'fokus'})">Legg til</button>
   </div>
 
-  <div class="preview">
+  <div id="preview" class="preview">
   <h1>{{ title }}</h1>
   <pre>{{ description }}</pre>
 
@@ -81,12 +135,6 @@ export default {
     </tr>
 
   </table>
-  </div>
-  <div>
-  <button @click="exportText">Export text</button>
-  <button @click="exportJson">Export json</button>
-  <button @click="save">Save</button>
-  <button @click="load">Load</button>
   </div>
 </template>
 
@@ -108,20 +156,44 @@ export default {
 </template>-->
 
 <style scoped>
-table.preview {
+
+nav {
+  background: #2c3e50;
+  height: 30px;
+  width: 50%;
+  padding: 0 2rem;
+  z-index: 1000;
+  top: 0;
+  position: fixed;
+  left: 25%;
+
+}
+table {
   border-collapse: collapse;
   width: 100%;
 }
 
+input {
+  width: 100%;
+}
+
 div.preview, div.edit {
+  margin-top: 30px;
   width: 95%
 }
-button {
-  margin: 0 auto;
+
+div.preview {
+  background: lightgoldenrodyellow;
+  padding: 5px;
+  resize: both;
+  overflow: auto;
 }
+
+
 textarea {
   width: 100%;
-  height: 100px;
+  height: 300px;
+  resize: none;
 }
 pre {
   white-space: pre-wrap;
