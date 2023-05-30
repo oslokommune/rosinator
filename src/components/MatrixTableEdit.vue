384a875c904ce31d441a960d5414649eb22c6ca3
Ivar Fredriksen <ivar.fredriksen@origo.oslo.kommune.no>
2023-05-30 09:24:15 +0200

    Remove buttons, just click in the table, add axis descriptors

--- src/components/MatrixTableEdit.vue
+++ src/components/MatrixTableEdit.vue
@@ -1,242 +1,167 @@
 <template>
   <table class="matrix_add">
+    <span><strong>Sannsynlighet</strong></span>
     <tr>
       <td>Svært stor</td>
-      <td class="yellow">
-
-
-        <button @click="pushHendelse('5','1')">  Velg   </button>
+      <td @click="pushHendelse('5','1')" class="yellow">
         <span v-if="hendelse.sannsynlighet === '5' && hendelse.konsekvens === '1'">
           {{ index + 1 }}
         </span>
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('5','2')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '5' && hendelse.konsekvens === '2'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('5','2')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('5','3')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '5' && hendelse.konsekvens === '3'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('5','3')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('5','4')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '5' && hendelse.konsekvens === '4'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('5','4')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('5','5')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '5' && hendelse.konsekvens === '5'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('5','5')">  Velg   </button>
-
       </td>
     </tr>
     <tr>
       <td>Stor</td>
-      <td class="green">
+      <td @click="pushHendelse('4','1')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '4' && hendelse.konsekvens === '1'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('4','1')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('4','2')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '4' && hendelse.konsekvens === '2'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('4','2')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('4','3')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '4' && hendelse.konsekvens === '3'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('4','3')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('4','4')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '4' && hendelse.konsekvens === '4'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('4','4')">  Velg   </button>
-
       </td>
-      <td class="red">
-
+      <td @click="pushHendelse('4','5')" class="red">
             <span v-if="hendelse.sannsynlighet === '4' && hendelse.konsekvens === '5'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('4','5')">  Velg   </button>
-
       </td>
     </tr>
     <tr>
       <td>Moderat</td>
-      <td class="green">
+      <td @click="pushHendelse('3','1')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '3' && hendelse.konsekvens === '1'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('3','1')">  Velg   </button>
-
       </td>
-      <td class="green">
+      <td @click="pushHendelse('3','2')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '3' && hendelse.konsekvens === '2'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('3','2')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('3','3')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '3' && hendelse.konsekvens === '3'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('3','3')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('3','4')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '3' && hendelse.konsekvens === '4'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('3','4')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('3','5')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '3' && hendelse.konsekvens === '5'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('3','5')">  Velg   </button>
-
       </td>
     </tr>
     <tr>
       <td>Liten</td>
-      <td class="green">
+      <td @click="pushHendelse('2','1')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '2' && hendelse.konsekvens === '1'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('2','1')">  Velg   </button>
-
       </td>
-      <td class="green">
+      <td @click="pushHendelse('2','2')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '2' && hendelse.konsekvens === '2'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('2','2')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('2','3')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '2' && hendelse.konsekvens === '3'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('2','3')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('2','4')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '2' && hendelse.konsekvens === '4'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('2','4')">  Velg   </button>
-
       </td>
-      <td class="red">
+      <td @click="pushHendelse('2','5')" class="red">
 
             <span v-if="hendelse.sannsynlighet === '2' && hendelse.konsekvens === '5'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('2','5')">  Velg   </button>
-
       </td>
     </tr>
     <tr>
       <td>Meget Liten</td>
-      <td class="green">
+      <td @click="pushHendelse('1','1')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '1' && hendelse.konsekvens === '1'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('1','1')">  Velg   </button>
-
       </td>
-      <td class="green">
+      <td @click="pushHendelse('1','2')" class="green">
 
             <span v-if="hendelse.sannsynlighet === '1' && hendelse.konsekvens === '2'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('1','2')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('1','3')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '1' && hendelse.konsekvens === '3'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('1','3')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('1','4')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '1' && hendelse.konsekvens === '4'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('1','4')">  Velg   </button>
-
       </td>
-      <td class="yellow">
+      <td @click="pushHendelse('1','5')" class="yellow">
 
             <span v-if="hendelse.sannsynlighet === '1' && hendelse.konsekvens === '5'">
               {{ index + 1 }}
             </span>
-
-          <button @click="pushHendelse('1','5')">  Velg   </button>
-        
       </td>
     </tr>
     <tr>
@@ -246,6 +171,14 @@
       <td>Moderat</td>
       <td>Alvorlig</td>
       <td>Svært alvorlig</td>
+    </tr>
+    <tr class="no-border">
+      <td class="no-border"></td>
+      <td class="no-border"></td>
+      <td class="no-border"></td>
+      <td class="no-border"></td>
+      <td class="no-border"></td>
+      <td class="no-border" style="text-align:right"><bold>Konsekvens</bold></td>
     </tr>
   </table>
 </template>
